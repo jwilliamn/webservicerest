815672b2cbb327b311da88a64ad86462099e3704
J. Williamn <hwilliamnq@gmail.com>
2018-02-02 16:45:56 -0500

    facerecognition done

--- faceRecognitionAPI/facerecognition/faceapi/facerec.py
+++ faceRecognitionAPI/facerecognition/faceapi/facerec.py
@@ -1,7 +1,7 @@
 import cv2
 import os
 import numpy as np
-#import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 
 from django.conf import settings
 
@@ -31,8 +31,16 @@ def detect_face(img):
 	if (len(faces) == 0):
 		return None, None
 
+	
+	## detecting all faces
+	#print("Found {0} faces!".format(len(faces)))
+	## Draw a rectangle around the faces
+	#for (x, y, w, h) in faces:
+	#	cv2.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 2)
+
 	# Assuming that ther is only one face
 	(x, y, w, h) = faces[0]
+
 
 	# return only the face part of the image
 	return gray[y:y + w, x:x + h], faces[0]
@@ -74,8 +82,10 @@ def prepare_training_data(data_folder_path):
 			image = cv2.imread(image_path)
 
 			# some displays
-			cv2.imshow("Training on image...", image)
-			cv2.waitKey(100)
+			#cv2.imshow("Training on image...", image)
+			#cv2.waitKey(100)
+			#plt.imshow(image)
+			#plt.show()
 
 			# detect face
 			face, rect = detect_face(image)
@@ -85,8 +95,53 @@ def prepare_training_data(data_folder_path):
 				faces.append(face)
 				labels.append(label)
 
-	cv2.destroyAllWindows()
-	cv2.waitKey(1)
-	cv2.destroyAllWindows()
+	#cv2.destroyAllWindows()
+	#cv2.waitKey(1)
+	#cv2.destroyAllWindows()
 
 	return faces, labels
+
+
+def train_faces(faces, labels):
+	# Create the lbph face recognizer
+	face_recognizer = cv2.face.LBPHFaceRecognizer_create()
+	#or use EigenFaceRecognizer by replacing above line with 
+	#face_recognizer = cv2.face.createEigenFaceRecognizer()
+
+	#or use FisherFaceRecognizer by replacing above line with 
+	#face_recognizer = cv2.face.createFisherFaceRecognizer()
+	face_recognizer.train(faces, np.array(labels))
+
+	return face_recognizer
+
+
+# Funtion to draw rectangle on images
+def draw_rectangle(img, rect):
+	(x, y, w, h) = rect
+	cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+# Function to draw text on given image
+def draw_text(img, text, x, y):
+	cv2.putText(img, text, (x, y), cv2.FONT_HERSHEY_PLAIN, 1.5, (0, 255, 0), 2)
+
+# prediction of the person given an image
+def predict(test_img, face_recog):
+	img = test_img.copy()
+	# detect face from the image
+	face, rect = detect_face(img)
+
+	# predict the image using our face recognizer
+	label = face_recog.predict(face)
+	print("label:", label)
+
+	# get name of respective label returned by face recognizer
+	label_text = subjects[max(label)]
+
+	# draw_rectangle around the face detected and the name of the predicted person
+	draw_rectangle(img, rect)
+	draw_text(img, label_text, rect[0], rect[1]-5)
+
+	#plt.imshow(img)
+	#plt.show()
+
+	return img, label, label_text
--- faceRecognitionAPI/facerecognition/faceapi/views.py
+++ faceRecognitionAPI/facerecognition/faceapi/views.py
@@ -8,26 +8,68 @@ from rest_framework.response import Response
 from rest_framework import views
 from django.http import JsonResponse
 
-from faceapi.facerec import prepare_training_data
+import cv2
+import matplotlib.pyplot as plt
+from faceapi.facerec import prepare_training_data, train_faces, predict
 
 
 # Load data directories
 dir_path = os.path.join(settings.MEDIA_ROOT, 'training-data')
 
+# Write results to output
+res_path = os.path.join(settings.OUTPUT_ROOT)
+
 # Create your views here.
 class IncredibleView(views.APIView):
-    def get(self, request):
-        # Get the model input
-        print("Preparing data...")
-        faces, labels = prepare_training_data(dir_path)
-        print("Total faces: ", len(faces))
-        print("Total labels: ", len(labels))
+	def get(self, request):
+		# Get the model input
+		print("Preparing data...")
+		faces, labels = prepare_training_data(dir_path)
+		print("Total faces: ", len(faces))
+		print("Total labels: ", len(labels))
 
-        # Perform the complex calculations
-        complex_result = len(faces) + len(labels)
-        print("complex_result: ", complex_result)
+		# Perform the complex calculations
+		complex_result = len(faces) + len(labels)
+		print("complex_result: ", complex_result)
 
-        # Return it in your custom format
-        return JsonResponse({
-            "complex_result": complex_result,
-            })
+		# trainig of the images
+		face_recognizer = train_faces(faces, labels)
+
+		# Prediction of images
+		print("Predicting images...")
+
+		# load test images
+		test_img1 = cv2.imread(os.path.join(settings.MEDIA_ROOT, 'test-data/test1.jpg'))
+		test_img2 = cv2.imread(os.path.join(settings.MEDIA_ROOT, 'test-data/test2.jpg'))
+
+		# perform a prediction
+		predicted_img1, lb, lbtxt = predict(test_img1, face_recognizer)
+		predicted_img2, _, _ = predict(test_img2, face_recognizer)
+
+		# create a figure of 2 plots (one for each test image)
+		#f, (ax1, ax2) = plt.subplots(1, 2, figsize=(10,5))
+
+		# display test image1 and image2
+		#plt.imshow(cv2.cvtColor(predicted_img1, cv2.COLOR_BGR2RGB))
+		#plt.show()
+		#plt.close()
+		cv2.cvtColor(predicted_img1, cv2.COLOR_BGR2RGB)
+		cv2.cvtColor(predicted_img2, cv2.COLOR_BGR2RGB)
+		#ax1.imshow(cv2.cvtColor(predicted_img1, cv2.COLOR_BGR2RGB))
+		#ax2.imshow(cv2.cvtColor(predicted_img2, cv2.COLOR_BGR2RGB))
+
+		# display both images
+		#cv2.imshow("Tom cruise test", predicted_img1)
+		#cv2.imshow("Shahrukh Khan test", predicted_img2)
+		#cv2.waitKey(0)
+		#cv2.destroyAllWindows()
+		#cv2.waitKey(1)
+		#cv2.destroyAllWindows()
+		cv2.imwrite(os.path.join(res_path, "out1.png"), predicted_img1)
+
+		# Return it in your custom format
+		return JsonResponse({
+			"complex_result": complex_result,
+			"label": lb,
+			"label text": lbtxt
+		})
